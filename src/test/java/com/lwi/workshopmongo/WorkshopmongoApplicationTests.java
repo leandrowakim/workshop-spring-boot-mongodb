3479fe7e76374b9b9077d4a4377470a36e50191e
leandrowakim <leandrowakim@gmail.com>
2021-05-02 14:43:01 -0300

    Primeiro commit projeto criado

---
+++ src/test/java/com/lwi/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
